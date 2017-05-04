ab8ee06afead840a6e803dc86080c3113a7badfe
Deepak Kumar <deepaknasa@gmail.com>
2017-05-04 22:24:38 +1000

    remove unnecessary files

--- FullStackTemplate/wwwroot/scripts/app/tt/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'template-app',
-  template: `<div>
-                <ng-content select="[dynamic-content]"></ng-content>
-            </div>`,
-})
-export class AppComponent  { name = 'Angular'; }
